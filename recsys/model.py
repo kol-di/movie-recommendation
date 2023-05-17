940bb882eaf2a098db0fa8b25a030591f735edc0
kol-di <60890747+kol-di@users.noreply.github.com>
2023-05-17 15:43:40 +0300

    user/number of recommendations out of bounds sanity check

--- recsys/model.py
+++ recsys/model.py
@@ -12,15 +12,26 @@ class ALS:
         assert isinstance(self.model, Union[cpu_ALS_cls, gpu_ALS_cls]), "Expected implicit ALS model instance"
         self.interactions = ALS.read_pkl(interactions_pkl_path)
         assert isinstance(self.interactions, csc_matrix), "Expected scipy csc matrix instance"
+        # Save max allowed number of items and users for further checks
+        self.max_users = self.interactions.shape[0]
+        self.max_items = self.interactions.shape[1]
 
     def get_user_recommendations(self, user_id: int, num_recs: int) -> list:
         """
         Get N recommendations for specified user id
         """
+        assert user_id < self.max_users, f"User ID out of range, max allowed ID is {self.max_users}"
+        assert num_recs < self.max_items, f"Too many recommendations requested, max allowed number is {self.max_items}"
+
         # Create user_items matrix for the user
         user_items = self.interactions[user_id]
         # Get user recommendations in score decreasing orders
-        recommendations = self.model.recommend(user_id, user_items.tocsr(), N=num_recs)[0].tolist()
+        recommendations = self.model.recommend(
+            user_id,
+            user_items.tocsr(),
+            N=num_recs,
+            filter_already_liked_items=True)[0].tolist()
+        for
 
         return recommendations
 
