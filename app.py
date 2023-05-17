e35e41bd8b5475eceafd82582576a3185e4e6598
kol-di <60890747+kol-di@users.noreply.github.com>
2023-05-17 13:55:46 +0300

    comments and unexpected behaviour check

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, abort
 from forms import RecommendForm
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def root():
+    # Redirect from app root to recommendations page
     return redirect(url_for('get_user_recommendations'))
 
 
 @app.route('/recommend', methods=['GET', 'POST'])
 def get_user_recommendations():
     form = RecommendForm(request.form)
+
     match request.method:
         case 'GET':
+            # On first access return basic template with a form
             return render_template('recommend.html', form=form)
+
         case 'POST':
+            # On form submission return template with the form and submission results
             if form.validate_on_submit():
                 user_id = 17
                 rec = ['abc', 'def', 'dcsd', 'dfcsd', 'sdcsd', 'sdcs']
@@ -23,9 +28,10 @@ def get_user_recommendations():
                     'user_id': user_id,
                     'rec': rec
                 }
-
                 return render_template('result.html', form=form, result=result)
+            abort(422)  # Return Unprocessable Entity error for invalid form
 
 
+# Execution entrypoint
 if __name__ == '__main__':
     app.run()
